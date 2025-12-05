f1d292361eb11fba1e1786cb921a37caefa6a752
upfive <metayoo@outlook.com>
2025-12-05 19:36:10 +0800

    update

--- src/main/java/com/pregnant/health/management/config/LoginInterceptor.java
+++ src/main/java/com/pregnant/health/management/config/LoginInterceptor.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import java.io.IOException;
 import java.io.PrintWriter;
 
 @Component
